9a34a3e6cec84432e0a1d1b6e366c6c2223aed30
King Baron ™ <85443712+Alfa00006@users.noreply.github.com>
2022-02-09 12:05:34 +0300

    Update helper.py

--- plugins/helper.py
+++ plugins/helper.py
@@ -5,7 +5,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from database.lang_utils import get_message as gm, get_message
 
 
-@Client.on_message(filters.command("help"))
+@Client.on_message(filters.command(["help", yardım"]))
 async def help_cmds_(_, message: Message):
     chat_id = message.chat.id
     user_id = message.from_user.id
@@ -18,7 +18,8 @@ async def help_cmds_(_, message: Message):
     keyboard.clear()
 
 
-__cmds__ = ["help"]
+__cmds__ = ["help", "yardım"]
 __help__ = {
-    "help": "help_help"
+    "help": "help_help", 
+    "yardım" "yardım_yardım" 
 }
