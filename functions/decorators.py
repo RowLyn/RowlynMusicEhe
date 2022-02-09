6a213fb5d4d5e8a7070b1d06bcc81a5a97002d8e
King Baron ™ <85443712+Alfa00006@users.noreply.github.com>
2022-02-09 11:45:37 +0300

    Update decorators.py

--- functions/decorators.py
+++ functions/decorators.py
@@ -105,3 +105,16 @@ def check_player(func: Callable) -> Callable:
             return await func(client, message)
         return await Bot().send_message(chat_id, "player_is_nonactive")
     return wrapper
+
+# Utils Helper
+def humanbytes(size):
+    """baytları Baytlara Dönüştür ki İnsan Okuyabilsin"""
+    if not size:
+        return ""
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
