4d57bd2829da30b8fbbcfef572eec0f6ca75adf3
King Baron ™ <85443712+Alfa00006@users.noreply.github.com>
2022-02-09 15:17:03 +0300

    Update configs.py

--- configs.py
+++ configs.py
@@ -15,7 +15,6 @@ class Configs:
     API_ID = int(getenv("API_ID", "0"))
     API_HASH = getenv("API_HASH", "abc123")
     BOT_TOKEN = getenv("BOT_TOKEN", "123:abc")
-    BOT_USERNAME = getenv("BOT_USERNAME", "Hopjazzmusic_bot")
     OWNER_ID = int(getenv("OWNER_ID", "0123"))
     SESSION = getenv("SESSION", "session")
     CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/Sohbetdestek")
