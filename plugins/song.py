c4a6120f583bfa0033174a3da066fe52834e4265
King Baron ™ <85443712+Alfa00006@users.noreply.github.com>
2022-02-09 11:46:32 +0300

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -18,7 +18,7 @@ from pyrogram.types import Message
 from youtube_search import YoutubeSearch
 from yt_dlp import YoutubeDL
 
-from functions.stats_utils import humanbytes
+from functions.decorators import humanbytes
 
 
 
